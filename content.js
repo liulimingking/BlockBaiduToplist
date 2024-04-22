e949dd182a4f742a64e0e9e65aa1fde6dcf6511e
liulimingking <75844487+liulimingking@users.noreply.github.com>
2024-04-22 10:42:48 +0800

    添加注释

--- content.js
+++ content.js
@@ -1,9 +1,11 @@
 const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 let mo = new MutationObserver(function () {
-    Array.from(document.querySelectorAll('div.opr-toplist1-table_544Ty'), item => { 
+    //搜索页
+	Array.from(document.querySelectorAll('div.opr-toplist1-table_544Ty'), item => { 
         item.style.display = "none"; 
     });
-    Array.from(document.querySelectorAll('ul.s-news-rank-content'), item => { 
+	//首页
+    Array.from(document.querySelectorAll('ul.s-hotsearch-content'), item => { 
       item.style.display = "none"; 
   });
 });
